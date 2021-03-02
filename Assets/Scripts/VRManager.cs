b9b2de9369e38d02964565afc3c4d10763bcfd8f
kit <kitbdev@gmail.com>
2021-03-01 22:13:35 -0500

    install packages

---
+++ Assets/Scripts/VRManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRManager : MonoBehaviour
+{
+    // todo check if vr is enabled
+    // 2d mode switch
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
